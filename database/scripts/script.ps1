6cecbf3d1c44eb480f0b2f60b739304167264191
amthomas46 <antho@microsoft.com>
2021-03-23 22:05:03 +0000

    first module updates

--- database/scripts/script.ps1
+++ database/scripts/script.ps1
@@ -5,11 +5,11 @@ param (
 
 $password = Read-Host "Azure SQL Password" -asSecureString
 
-$sqlPackage="C:\Tools\SqlPackage\sqlpackage-win7-x64-en-US-15.0.4897.1\sqlpackage.exe"
+$sqlPackage="C:\SqlPackage\sqlpackage.exe"
 $dacpac="bus-db.dacpac"
 
 $PwdPointer = [Runtime.InteropServices.Marshal]::SecureStringToBSTR($password)
 $PlainTextPassword = [Runtime.InteropServices.Marshal]::PtrToStringAuto($PwdPointer)
 [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($PwdPointer)
 
-Invoke-Expression "$sqlPackage /a:script /op:script.sync.sql /sf:$dacpac /tcs:""Data Source=$server.database.windows.net;Initial Catalog=bus_db;UID=$username;PWD=$PlainTextPassword"""
+Invoke-Expression "$sqlPackage /a:script /op:script.sync.sql /sf:$dacpac /tcs:""Data Source=$server.database.windows.net;Initial Catalog=bus-db;UID=$username;PWD=$PlainTextPassword"""
