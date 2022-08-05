14fbc2b7e61d3e1b35820f7c8e553ca40854fa3b
Jay Miller <kjaymiller@gmail.com>
2022-08-05 10:02:14 -0700

    remove helping logic from __init__.py

--- azure-function/python/GetBusData/__init__.py
+++ azure-function/python/GetBusData/__init__.py
@@ -1,103 +1,48 @@
-import datetime
 import logging
 import os
-import requests
-import json
-import pyodbc
-from datetime import datetime as dt
+from typing import Any
+
 import azure.functions as func
+import pyodbc
+import json
+import requests
 
-AZURE_CONN_STRING = str(os.environ["AzureSQLConnectionString"])
-GTFS_RT_FEED = str(os.environ["RealTimeFeedUrl"])
-LOGIC_APP_URL = str(os.environ["LogicAppUrl"])
+from datetime import datetime
+from .bus_data_process import (
+    get_bus_data_from_feed,
+    get_geo_fences,
+    get_monitored_format,
+    get_monitored_routes,
+    get_route_id,
+    trigger_logic_app,
+)
 
-def main(GetBusData: func.TimerRequest) -> func.HttpResponse:
-    ## Get the routes we want to monitor
-    routes = GetMonitoredRoutes()
+AZURE_CONN_STRING: str = os.environ["AzureSQLConnectionString"]
+GTFS_REAL_TIME_FEED: str = os.environ["RealTimeFeedUrl"]
+LOGIC_APP_URL: str = os.environ.get("LogicAppUrl", "")
+
+
+def main(GetBusData: func.TimerRequest) -> None:
+    """Retrieve the routes we want to monitor from the SQL Database"""
+    conn: str = pyodbc.connect(AZURE_CONN_STRING)
+    monitored_routes: list[int] = get_monitored_routes(conn)
+    logging.info(f"{len(monitored_routes)} routes to check against")
+    entities = get_bus_data_from_feed(GTFS_REAL_TIME_FEED)['entity']
+    logging.info(monitored_routes)
+
+    # reformat the bus_feed to match the format of the monitored_routes
+    monitored_buses = [get_monitored_format(bus['vehicle']) for bus in entities if get_route_id(bus) in monitored_routes]
+    logging.info(f"{len(entities)} buses found. {len(monitored_buses)} buses monitored.")
     
-    ## Get the real-time bus location feed
-    feed = GetRealTimeFeed()
+    if not monitored_buses:
+        logging.info("No Monitored Bus Routes Detected")
+        return
+
+    geo_fences = get_geo_fences(conn, monitored_buses) or list()
+    logging.info(f"{geo_fences=}")
+
+    ## Send notifications. UnComment the Lines below in the "Deploy and monitor notifications using Azure Logic Apps" Exercise
     
-    ## Filter only the routes we want to monitor
-    buses = [f for f in feed if int(f["RouteId"]) in routes]
-
-    logging.info('Received {0} buses positions, found {1} buses in monitored routes'.format(len(feed), len(buses)))
-
-    ## Push data to Azure SQL and get the activated geofences
-    activatedGeofences = ProcessGeoFences(buses)
-
-    ## Send notifications 
-    for gf in activatedGeofences:
-        logging.info('Vehicle %i, route %s, %sing GeoFence %s at %s UTC', gf["VehicleId"], gf["RouteId"], gf["GeoFenceStatus"], gf["GeoFence"], gf["TimestampUTC"])
-        TriggerLogicApp(gf)
-
-def GetRealTimeFeed():
-    response = requests.get(GTFS_RT_FEED)
-    entities = json.loads(response.text)['entity']
-    busData = []
-    for entity in entities:
-        v = entity['vehicle']
-        busDetails = {
-            "DirectionId": v['trip']['direction_id'],
-            "RouteId": v['trip']['route_id'],
-            "VehicleId": v['vehicle']['id'],
-            "Position": {
-                "Latitude": v['position']['latitude'],
-                "Longitude": v['position']['longitude']
-            },
-            "TimestampUTC": dt.utcfromtimestamp(v['timestamp']).isoformat(sep=' ')
-        }
-        busData.append(busDetails)    
-    return busData
-
-def GetMonitoredRoutes():
-    result = executeQueryJSON('web.GetMonitoredRoutes')    
-    return [r["RouteId"] for r in result]
-
-def ProcessGeoFences(payload):    
-    result = {}
-    if payload:
-        result = executeQueryJSON('web.AddBusData', payload)
-        logging.info('Found %i buses activating a geofence',len(result))
-    return result
-
-def TriggerLogicApp(geoFence):
-    content = {
-        "value1": str(geoFence["VehicleId"]), 
-        "value2": str(geoFence["GeoFenceStatus"])
-    }
-
-    logging.info("Calling Logic App webhook for {0}".format(geoFence["VehicleId"]))
-
-    params = { 
-        "Content-type": "application/json" 
-    }
-
-    response = requests.post(LOGIC_APP_URL, json=content, headers=params)
-    if response.status_code != 202:
-        logging.info('Error calling Logic App: {0}'.format(response.status_code))        
-    else:
-        logging.info("[%i/%i/%i] WebHook called successfully", geoFence["VehicleId"], geoFence["DirectionId"], geoFence["GeoFenceId"])
-
-def executeQueryJSON(procedure, payload=None):
-    result = {}
-    try: 
-        conn = pyodbc.connect(AZURE_CONN_STRING)
-        
-        with conn.cursor() as cursor:
-            if payload:            
-                cursor.execute(f"EXEC {procedure} ?", json.dumps(payload))
-            else:
-                cursor.execute(f"EXEC {procedure}")
-
-            result = cursor.fetchone()[0]
-            
-            if result:
-                result = json.loads(result)                           
-            else:
-                result = {}        
-        
-    finally:
-        pass
-
-    return result
+    # for fence in geo_fences:
+    #    logging.info(f"Vehicle {fence['vehicle_id']}, route {fence['route_id']}, status: {fence['geofence_status']} at {fence['timestamp']} UTC")
+    #    trigger_logic_app(fence)
